e4c3cbc0eebe98cafde42dba1153c95c0b388ffe
selim <selimdev00@gmail.com>
2023-05-11 13:55:04 +0500

    [playing]: playing around with python

---
+++ main.py
@@ -0,0 +1,18 @@
+# надо установить модуль с командой `pip install python-weather`
+
+import python_weather # https://pypi.org/project/python-weather/
+
+import asyncio
+import os
+
+async def getweather():
+  async with python_weather.Client(unit=python_weather.IMPERIAL) as client:
+    place = 'Ashgabat'
+    weather = await client.get(place)
+    print('Weather in ' + place + ': ' + str(weather.current.temperature) + "F, " + weather.current.description + ' and ' + str(weather.current.kind))
+    
+if __name__ == '__main__':
+  if os.name == 'nt':
+    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+  
+  asyncio.run(getweather())
